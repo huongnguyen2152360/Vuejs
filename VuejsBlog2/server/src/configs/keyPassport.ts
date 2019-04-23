e4a31f1bd04502c59caf830557aa3d789f24fe65
Hương Nguyễn <huongnguyen2152360@gmail.com>
2019-04-23 17:02:52 +0700

    fb login

--- VuejsBlog2/server/src/configs/keyPassport.ts
+++ VuejsBlog2/server/src/configs/keyPassport.ts
@@ -1,7 +1,7 @@
 export default {
     facebook: {
-        clientID: "387427091853388",
-        clientSecret: "c72508a8b44ac346616aff6e9208345a",
+        clientID: "",
+        clientSecret: "",
         callbackURL: "https://localhost:3000/auth/facebook/callback",
         profileFields: ['id', 'name', 'profile_pic', 'email']
     }
